997228a5a180c93101a1a2224a0eb01f8e096abb
greenac <andre1226.dev@gmail.com>
2020-05-11 12:40:17 -0700

    Removing proxy through local backend. Traffic to artemis

--- server/index.ts
+++
@@ -1,6 +0,0 @@
-require("dotenv").config()
-
-import run from "./src/app"
-import logger from "./src/logger/logger"
-
-run().catch(error => logger.error("App failed with error:", error))
--- server/src/consts/artemis-urls.ts
+++
@@ -1,9 +0,0 @@
-interface IArtemisUrls {
-  AllActors: string
-}
-
-const artemisUrls: IArtemisUrls = {
-  AllActors: "/api/all-actors"
-}
-
-export default  artemisUrls
